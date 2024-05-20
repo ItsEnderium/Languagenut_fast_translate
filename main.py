443d148b957ad0b2b3077c2bb0e791a14e11ccd4
ENDERIUM <82263560+ItsEnderium@users.noreply.github.com>
2024-05-20 21:47:12 +0000

    Added a bunch of stuff, should be functional now

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-with open("input.txt", "r+")as file1,open("1.txt","r+") as file2:
+with open("input.txt","r+")as inp,open("1.txt","r+")as file1,open("2.txt","r+") as file2:
+    file1.truncate()
+    file1.write(inp.read().lower())
+    file1.seek(0)
+    lines=file1.readlines()
+    file1.seek(0)
+    file1.truncate()
+    zeros=len(str(len(lines)))
+    for i,line in enumerate(lines):
+        newl=line.replace('\t',' | ')
+        file1.write(f"{str(i+1).zfill(zeros)} | {newl}")
+    file1.seek(0)
     content=file1.read()
-    replaced_content=content.replace(" à","a").lower()
+    replaced_content=content.replace("à","a")
     replaced_content=replaced_content.replace("â","a")
     replaced_content=replaced_content.replace("ä","a")
     replaced_content=replaced_content.replace("æ","ae")
@@ -18,14 +29,17 @@ with open("input.txt", "r+")as file1,open("1.txt","r+") as file2:
     replaced_content=replaced_content.replace("ü","u")
     file2.write(replaced_content)
     file2.truncate()
+    file1.seek(0)
+    print(len(file1.readlines()),"items left.")
     while True:
-        search=input("Enter a string to search for in 1.txt: ")
+        search=input("Search for a string or number: ")
+        print()
         results=[]
         file2.seek(0)
-        for line in file2:
-            if search in line:
-                results.append(line)
-        print(len(results),"result"+("s"if len(results)!=1else""),"found")
+        for i in file2:
+            if search in i:
+                results.append(i.strip())
+        print(len(results)if len(results)!=0else"No","result"+("s"if len(results)!=1else""),"found"+(":"if len(results)!=0else"."))
         for i in results:
             print(i)
         if len(results)==1:
@@ -40,3 +54,6 @@ with open("input.txt", "r+")as file1,open("1.txt","r+") as file2:
             file1.seek(0)
             file1.write(f2)
             file1.truncate()
+            file1.seek(0)
+            print("Item removed,",len(file1.readlines()),"items left.")
+        print()
